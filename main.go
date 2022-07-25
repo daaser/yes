7bd83053515e29f1d48e573033ca091e88e86377
Doug Aaser <douglasaaser@gmail.com>
2022-07-25 07:04:34 -0400

    Disable GC in attempt for more performance - 6.09Gib/s

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bufio"
 	"os"
+	"runtime/debug"
 )
 
-const BUFSIZE = 64 * 1024 // 64 Kib
+const (
+	BUFSIZE    = 64 * 1024   // 64 Kib
+	TOTAL_DATA = 1024 * 1024 // 1024 Kib * 64 Kib = 64 Gib
+)
 
 func partition(buf []byte, mid int) ([]byte, []byte) {
 	return buf[0:mid], buf[mid:]
@@ -19,23 +23,23 @@ func fillBuffer(buffer, output []byte) []byte {
 	bufferSize := len(output)
 	copy(buffer[:bufferSize], output)
 
-	for bufferSize < len(buffer)/2 {
+	for ; bufferSize < len(buffer)/2; bufferSize *= 2 {
 		left, right := partition(buffer, bufferSize)
 		copy(right[:bufferSize], left)
-		bufferSize *= 2
 	}
 	return buffer[:bufferSize]
 }
 
 func main() {
+	debug.SetGCPercent(-1)
 	var (
 		buffer [BUFSIZE]byte
-		output = []byte("y\n")
+		output = []byte{'y', '\n'}
+		writer = bufio.NewWriter(os.Stdout)
 	)
 
 	filled := fillBuffer(buffer[:], output)
-	writer := bufio.NewWriter(os.Stdout)
-	for {
+	for i := 0; i < TOTAL_DATA; i++ {
 		writer.Write(filled)
 	}
 }
