20a2c655cb18072404b82b4233c2243eceaf04f6
Doug Aaser <douglasaaser@gmail.com>
2022-07-24 22:11:12 -0400

    Much better performance - 5.63GiB/s

--- main.go
+++ main.go
@@ -7,9 +7,35 @@ import (
 
 const BUFSIZE = 64 * 1024 // 64 Kib
 
+func partition(buf []byte, mid int) ([]byte, []byte) {
+	return buf[0:mid], buf[mid:]
+}
+
+func fillBuffer(buffer, output []byte) []byte {
+	if len(output) > len(buffer)/2 {
+		return output
+	}
+
+	bufferSize := len(output)
+	copy(buffer[:bufferSize], output)
+
+	for bufferSize < len(buffer)/2 {
+		left, right := partition(buffer, bufferSize)
+		copy(right[:bufferSize], left)
+		bufferSize *= 2
+	}
+	return buffer[:bufferSize]
+}
+
 func main() {
+	var (
+		buffer [BUFSIZE]byte
+		output = []byte("y\n")
+	)
+
+	filled := fillBuffer(buffer[:], output)
 	writer := bufio.NewWriter(os.Stdout)
 	for {
-		writer.Write([]byte("y\n"))
+		writer.Write(filled)
 	}
 }
